1142d258a4db0d4b48675624d5772749edfba648
xiaoyi1972 <xiaoyi19722006@126.com>
2025-12-11 06:47:06 +0800

    Your branch is up to date with 'origin/main'. 	modified:   .github/workflows/nightly.yaml 	modified:   config/boards/orangepi-5-max.sh 	modified:   config/suites/plucky.sh
     Changes not staged for commit:

--- config/suites/plucky.sh
+++ config/suites/plucky.sh
@@ -4,7 +4,7 @@ export RELEASE_NAME="Ubuntu 25.04 (Plucky Puffin)"
 export RELEASE_VERSION="25.04"
 
 export KERNEL_REPO="https://github.com/Joshua-Riek/linux-rockchip.git"
-export KERNEL_BRANCH="plucky"
+export KERNEL_BRANCH="noble"
 export KERNEL_FLAVOR="rockchip"
 
 export EXTRA_PPAS="jjriek/rockchip"
