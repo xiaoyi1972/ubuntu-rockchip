d7bc5b304160c098fcc7973b0a03f4b6400ab124
xiaoyi1972 <30370004+xiaoyi1972@users.noreply.github.com>
2025-12-25 18:03:23 +0800

    Update APT sources configuration in config-image.sh

--- scripts/config-image.sh
+++ scripts/config-image.sh
@@ -79,11 +79,53 @@ setup_mountpoint() {
 configure_apt_sources() {
     local root="$1"
     local suite="$2"
-    cat <<EOF > "$root/etc/apt/sources.list"
-deb http://mirrors.aliyun.com/ubuntu/ ${suite} main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ ${suite}-updates main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ ${suite}-security main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ ${suite}-backports main restricted universe multiverse
+
+    sudo cat << 'EOF' > "$root/etc/apt/sources.list.d/ubuntu.sources"
+# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
+# newer versions of the distribution.
+
+## Ubuntu distribution repository
+##
+## The following settings can be adjusted to configure which packages to use from Ubuntu.
+## Mirror your choices (except for URIs and Suites) in the security section below to
+## ensure timely security updates.
+##
+## Types: Append deb-src to enable the fetching of source package.
+## URIs: A URL to the repository (you may add multiple URLs)
+## Suites: The following additional suites can be configured
+##   <name>-updates   - Major bug fix updates produced after the final release of the
+##                      distribution.
+##   <name>-backports - software from this repository may not have been tested as
+##                      extensively as that contained in the main release, although it includes
+##                      newer versions of some applications which may provide useful features.
+##                      Also, please note that software in backports WILL NOT receive any review
+##                      or updates from the Ubuntu security team.
+## Components: Aside from main, the following components can be added to the list
+##   restricted  - Software that may not be under a free license, or protected by patents.
+##   universe    - Community maintained packages.
+##                 Software from this repository is only maintained and supported by Canonical
+##                 for machines with Ubuntu Pro subscriptions. Without Ubuntu Pro, the Ubuntu
+##                 community provides best-effort security maintenance.
+##   multiverse  - Community maintained of restricted. Software from this repository is
+##                 ENTIRELY UNSUPPORTED by the Ubuntu team, and may not be under a free
+##                 licence. Please satisfy yourself as to your rights to use the software.
+##                 Also, please note that software in multiverse WILL NOT receive any
+##                 review or updates from the Ubuntu security team.
+##
+## See the sources.list(5) manual page for further settings.
+Types: deb
+URIs: http://mirrors.aliyun.com/ubuntu-ports/
+Suites: plucky plucky-updates plucky-backports
+Components: main universe restricted multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+
+## Ubuntu security updates. Aside from URIs and Suites,
+## this should mirror your choices in the previous section.
+Types: deb
+URIs: http://mirrors.aliyun.com/ubuntu-ports/
+Suites: plucky-security
+Components: main universe restricted multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 EOF
 }
 
@@ -110,7 +152,7 @@ tar -xpJf "ubuntu-${RELEASE_VERSION}-preinstalled-${FLAVOR}-arm64.rootfs.tar.xz"
 
 setup_mountpoint $chroot_dir
 
-configure_apt_sources "$chroot_dir" "${SUITE}"
+# configure_apt_sources "$chroot_dir" "${SUITE}"
 
 chroot $chroot_dir apt-get update
 chroot $chroot_dir apt-get -y upgrade
