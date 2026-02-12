184172940a4b9743f7bff20255e29113bc5f14bd
sfqr0414 <sfqr0414@gmail.com>
2026-02-13 07:49:37 +0800

    Update plucky.sh

--- config/suites/plucky.sh
+++ config/suites/plucky.sh
@@ -3,8 +3,8 @@
 export RELEASE_NAME="Ubuntu 25.04 (Plucky Puffin)"
 export RELEASE_VERSION="25.04"
 
-export KERNEL_REPO="https://github.com/sfqr0414/linux-rockchip.git"
-#export KERNEL_REPO="https://github.com/Joshua-Riek/linux-rockchip.git"
+#export KERNEL_REPO="https://github.com/sfqr0414/linux-rockchip.git"
+export KERNEL_REPO="https://github.com/Joshua-Riek/linux-rockchip.git"
 export KERNEL_BRANCH="noble"
 export KERNEL_FLAVOR="rockchip"
 
