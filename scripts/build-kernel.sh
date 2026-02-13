06f3a28ee2348c4771314131ce991aad81e5ffd4
sfqr0414 <sfqr0414@gmail.com>
2026-02-13 08:16:47 +0800

    Change base image to public ECR for Ubuntu

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -131,7 +131,8 @@ if ! docker images | grep -q "${DOCKER_IMAGE}"; then
 # 定义 ARG（必须在 FROM 前）
 ARG UBUNTU_VERSION=25.04
 # 基础镜像
-FROM ubuntu:\${UBUNTU_VERSION}
+FROM public.ecr.aws/ubuntu/ubuntu:\${UBUNTU_VERSION}
+# FROM ubuntu:\${UBUNTU_VERSION}
 
 # 定义容器内需要的 ARG
 ARG UBUNTU_VERSION
