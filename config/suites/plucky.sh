67fcf5c5f33d1a44ce495f842989808760578034
sfqr0414 <sfqr0414@gmail.com>
2026-02-15 00:07:45 +0800

    Update kernel repository and branch in plucky.sh

--- config/suites/plucky.sh
+++ config/suites/plucky.sh
@@ -5,7 +5,8 @@ export RELEASE_VERSION="25.04"
 
 #export KERNEL_REPO="https://github.com/sfqr0414/linux-rockchip.git"
 export KERNEL_REPO="https://github.com/Joshua-Riek/linux-rockchip.git"
-export KERNEL_BRANCH="noble"
+#export KERNEL_BRANCH="noble"
+export KERNEL_BRANCH="armbian-import"
 export KERNEL_FLAVOR="rockchip"
 
 export EXTRA_PPAS="jjriek/rockchip"
